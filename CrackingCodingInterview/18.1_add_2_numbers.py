602ab13922a73a5155f9767ab9d4d3106333591a
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-02 01:59:12 -0700

    this is a hard problem, I am tryiing to solver with iteration

--- CrackingCodingInterview/18.1_add_2_numbers.py
+++ CrackingCodingInterview/18.1_add_2_numbers.py
@@ -3,4 +3,13 @@
 def foo(a, b):
     #perhaps translate to byte strings and use bit shifts
     #perhaps import c handlers and manipulate on low level
-     
+    #perhaps i can iterating thorugh a, then b and append some counter to a list and find the len of the list.
+
+    psudo code:
+    x = []
+        for i in range a:
+            append.x
+        for i in ranbe b:
+            append x.
+        len.x
+        
