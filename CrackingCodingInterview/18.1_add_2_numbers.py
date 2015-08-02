d0538775474e6b539bbe2c4ac452fc36bc36d712
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-02 01:46:34 -0700

    starting hard addition problem

---
+++ CrackingCodingInterview/18.1_add_2_numbers.py
@@ -0,0 +1,6 @@
+#add two numbers without using operators
+
+def foo(a, b):
+    #perhaps translate to byte strings and use bit shifts
+    #perhaps import c handlers and manipulate on low level
+     
