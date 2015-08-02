6fdc8b64ebc4d149bf2da4e87f7418341e94566e
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-01 20:00:26 -0700

    starting 2.1

---
+++ CrackingCodingInterview/2.1_remove_duplicates.py
@@ -0,0 +1,8 @@
+#remove duplicates from an unsorted linked list
+from classes.LinkedList import *
+
+
+def foo():
+# start at head
+# iterate through at test if there is a node infront of current with same value
+
