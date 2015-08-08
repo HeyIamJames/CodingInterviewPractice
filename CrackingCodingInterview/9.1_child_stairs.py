2161b60a3225d4dc81198844efa767a2667d1d10
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-08 11:18:35 -0700

    more psudo code

--- CrackingCodingInterview/9.1_child_stairs.py
+++ CrackingCodingInterview/9.1_child_stairs.py
@@ -8,6 +8,20 @@ child can run up the stairs.
 # psudo code:
 # try dividing staris by largest num, 3, for the modulos, try it against 2, then 1
 # then try entre set against 2, using above method, then call entire set agins 1, which would always be 1
+# if there is no remainder, dont call again for same subset
+
+# more psudo code:
+# if steps was 8
+# intialize a counter = 0
+# 8 % 3 = 2, counter += 1
+# 2 % 2 = 0, counter += 2 > I add 2 because there is no point checking against 1
+
+# call recusiveley on 2
+# 8 % 2 = 0, counter += 2
+
+# sum the counters:
+# 1 + 2 + 2 = 5
+
 
 
 def run(stairs):
