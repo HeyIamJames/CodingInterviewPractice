876ec984ccb9765ce99b223bdd1556c546af990f
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-08 11:04:24 -0700

    added psudo code

--- CrackingCodingInterview/9.1_child_stairs.py
+++ CrackingCodingInterview/9.1_child_stairs.py
@@ -5,7 +5,10 @@ Implement a method to count how many possible ways the
 child can run up the stairs.
 """
 
+# psudo code:
+# try dividing staris by largest num, 3, for the modulos, try it against 2, then 1
+# then try entre set against 2, using above method, then call entire set agins 1, which would always be 1
+
 
 def run(stairs):
     for steps in stairs:
-        
