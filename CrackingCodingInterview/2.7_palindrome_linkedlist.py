176ab12979c68002fe6bebbf71e23f9f8e66e999
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-15 09:43:24 -0700

    check if a linked list is a palindrome.

---
+++ CrackingCodingInterview/2.7_palindrome_linkedlist.py
@@ -0,0 +1,7 @@
+"""
+check if a linked list is a palindrome.
+"""
+
+#one way: check if what you pass is the reverse of what you are checkng by looking at last node
+#try to create the first list from the second begining at the tail of second from head of first back
+
