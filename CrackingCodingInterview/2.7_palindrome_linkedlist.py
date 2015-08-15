e5ab71a557fe455ddd5bd304413cb1dffd880355
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-15 09:49:46 -0700

    just reverse the list

--- CrackingCodingInterview/2.7_palindrome_linkedlist.py
+++ CrackingCodingInterview/2.7_palindrome_linkedlist.py
@@ -2,11 +2,13 @@
 check if a linked list is a palindrome.
 """
 
-#one way: check if what you pass is the reverse of what you are checkng by looking at last node
-#try to create the first list from the second begining at the tail of second from head of first back
+# reverse the list and check if the reverse is equal to the non reverse
 
 #psudo code:
-start at head of linked list 1 and tail of linked list 2
-for the length of the lists, check that the first lists next node is the other list 2 previous from the end of list 2 is equal
+# def foo(list):
+#   revrse the list
+#   check if the reversed list is the list
 
-
+def isPalindrome(LinkedList):
+	reverseL1 = reverseList(LinkedList)
+	return isEqual(LinkedList, reverseLinkedList)
