62b25a230b732720fcaf9f3c342cf50f9f722d86
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-15 09:45:40 -0700

    psudo code added

--- CrackingCodingInterview/2.7_palindrome_linkedlist.py
+++ CrackingCodingInterview/2.7_palindrome_linkedlist.py
@@ -5,3 +5,8 @@ check if a linked list is a palindrome.
 #one way: check if what you pass is the reverse of what you are checkng by looking at last node
 #try to create the first list from the second begining at the tail of second from head of first back
 
+#psudo code:
+start at head of linked list 1 and tail of linked list 2
+for the length of the lists, check that the first lists next node is the other list 2 previous from the end of list 2 is equal
+
+
