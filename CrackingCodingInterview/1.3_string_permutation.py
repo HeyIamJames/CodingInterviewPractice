005655b589cc61c4bc581239b5d7e0a7869d9436
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-14 21:55:01 -0700

    use sorted to check for permutation

--- CrackingCodingInterview/1.3_string_permutation.py
+++ CrackingCodingInterview/1.3_string_permutation.py
@@ -3,3 +3,8 @@ check if a string is a permutation of anohter string
 """
 
 #utalize sorted, perhaps check length first to make faster
+
+def Permutation(string1,string2):
+      if len(string1)!=len(string2):
+          return False
+      return sorted(string1) == sorted(string2)
