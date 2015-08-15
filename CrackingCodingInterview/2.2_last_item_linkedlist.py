c186039edf4935ebb1dffd7354b2c0631ae34c32
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-14 21:40:50 -0700

    returns last node

--- CrackingCodingInterview/2.2_last_item_linkedlist.py
+++ CrackingCodingInterview/2.2_last_item_linkedlist.py
@@ -4,5 +4,17 @@ find the last item in a singly linked list
 
 #keep checking list until there is node has no next
 
-def lastnode(linked_list):
-    while 
+def lastnode(linked_list, k):
+    if k <= 0:
+        return None
+    pointer2 = linkedlist.head
+    for i in range(k-1):
+        if pointer2.next != None:
+            pointer2 = pointer2.next
+        else:
+            return "loop"
+    pointer1 = linkedlist.head
+    while pointer2.next != None:
+        pointer2 = pointer2.next
+        pointer1 = pointer1.next
+    return pointer1
