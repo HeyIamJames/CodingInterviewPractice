f1d409b5f172890fc098ec47b81b8d365d5b94de
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-16 12:03:05 -0700

    starting x stacks in an array

---
+++ CrackingCodingInterview/3.1_3stacks_1array.py
@@ -0,0 +1,8 @@
+"""
+Create an array utalizing 3 stacks.
+"""
+
+#make an array class with attribues of size and number of stacks
+#implement stack functions of push pop on each stack
+
+Psudo Code:
