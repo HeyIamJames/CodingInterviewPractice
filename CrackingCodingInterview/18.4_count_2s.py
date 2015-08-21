fe905d9cf9ebb26208fd241436fb718a9071be7a
HeyIamJames <jameshemmaplardh@gmail.com>
2015-08-21 12:19:29 -0700

    psudo coding

--- CrackingCodingInterview/18.4_count_2s.py
+++ CrackingCodingInterview/18.4_count_2s.py
@@ -2,4 +2,13 @@
 write a method to count the number of 2's in range n
 """
 
-psudo code:
+# psudo code:
+# given n
+# iterate through range n
+# cast of of the numbers into strings
+# start a counter
+# if anything in string = 2, counter += 1
+# counter is number of 2's
+
+def count2():
+    
