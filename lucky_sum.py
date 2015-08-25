bec68fc7d32a25ae6b49d43ff4db715ad4f56956
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-08-24 20:18:38 -0700

    Update lucky_sum.py

--- lucky_sum.py
+++ lucky_sum.py
@@ -6,3 +6,12 @@ lucky_sum(1, 2, 3) → 6
 lucky_sum(1, 2, 13) → 3
 lucky_sum(1, 13, 3) → 1
 
+def lucky_sum(a, b, c):
+  if a == 13:
+    return 0
+	elif b == 13:
+		return a
+	elif c == 13:
+		return a + b
+	else:
+		return a + b + c
