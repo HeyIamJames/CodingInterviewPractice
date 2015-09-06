116d3610eb5c8cebbee857301c49614b7d8d4258
HeyIamJames <jameshemmaplardh@gmail.com>
2015-09-06 01:28:07 -0700

    added recursive

--- CrackingCodingInterview/9.4_subsetsofset.py
+++ CrackingCodingInterview/9.4_subsetsofset.py
@@ -6,3 +6,14 @@ return all the subsets of a set
 #iterate through each subset within the set
 #if found make a copy in a new set or reuturn individually
 
+
+def subset(seq):
+    main_set = list(seq)
+    if len(main_set) < 1:
+        yield main_set
+        return
+
+    element = [main_set.pop()]
+    for sub_set in subsets(main_set):
+        yield sub_set
+        yield sub_set + element
