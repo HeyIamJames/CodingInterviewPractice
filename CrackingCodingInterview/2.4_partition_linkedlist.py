4cb132e9697e0394791d433928be4848313b837a
HeyIamJames <jameshemmaplardh@gmail.com>
2015-09-08 15:51:15 -0700

    psudo code

--- CrackingCodingInterview/2.4_partition_linkedlist.py
+++ CrackingCodingInterview/2.4_partition_linkedlist.py
@@ -3,3 +3,9 @@ partition a linked list around x
 all nodes < x are sorted before x
 """
 
+# psudo code:
+# while current < x, prepend to head
+# anything less than x is before anyhting larger
+
+def partition():
+    
