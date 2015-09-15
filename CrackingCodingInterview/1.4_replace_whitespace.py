7f20aa725f7ab209a0492bbbef8a50cc41cdd7f1
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-09-15 15:27:02 -0700

    Update 1.4_replace_whitespace.py

--- CrackingCodingInterview/1.4_replace_whitespace.py
+++ CrackingCodingInterview/1.4_replace_whitespace.py
@@ -3,6 +3,4 @@ Replace all whitespace in a string with '%20'
 """
 
 def replace(string):
-    for i in string:
-        string.replace("", %20)
-    return string
+    return string.replace(' ', '%20')
