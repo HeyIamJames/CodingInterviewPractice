74d66659d078b9235a444ed7f8f1e942abf5d207
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-09-14 20:46:15 -0700

    Update 1.2_void_reverse.cpp

--- CrackingCodingInterview/1.2_void_reverse.cpp
+++ CrackingCodingInterview/1.2_void_reverse.cpp
@@ -1,24 +1,30 @@
-using std::cout;
-using std::endl;
-
-void Question1_2::reverse(char* str) 
-{
-    char *ptrEnd = str;
+#include <stdio.h>
+#include <string.h>
+ 
+#define MAXSIZE 256
+ 
+void reverse(char *str){
+    char *str_begin = str;
+    char *str_end = str+strlen(str)-1;  // Point to the last char
     char temp;
-
-    if (str) 
-    {
-        while (*ptrEnd) 
-        {
-            ptrEnd++;
-        }
-        ptrEnd--;
-
-        while (str < ptrEnd) 
-        {
-            temp = *str;
-            *str++ = *ptrEnd;
-            *ptrEnd-- = temp;
-        }
+    while(str_begin < str_end){
+        temp = *str_begin;
+        *(str_begin++) = *str_end;
+        *(str_end--) = temp;
     }
+    return;
+}
+ 
+int main(int argc, char * argv[]){
+    if(argc != 2){
+        printf("Unsuitable arguments!\n");
+        return 1;
+    }
+    char str[MAXSIZE];
+    str[MAXSIZE-1] = '\0';
+    strncpy(str,argv[1],MAXSIZE-1);
+    printf("ready to work: %s\n",str);
+    reverse(str);
+    printf("Work result: %s\n",str);
+    return 0;
 }
