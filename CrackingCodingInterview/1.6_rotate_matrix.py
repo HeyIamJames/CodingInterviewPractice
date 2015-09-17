0a6404805f2c380c0f13781156b1158dbacdf37b
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-09-17 15:40:04 -0700

    Update 1.6_rotate_matrix.py

--- CrackingCodingInterview/1.6_rotate_matrix.py
+++ CrackingCodingInterview/1.6_rotate_matrix.py
@@ -1,3 +1,10 @@
 """
 rotate an n by n matrix 90 degrees
 """
+
+def rotate(matrix):
+    matrix_new = copy.deepcopy(matrix)
+    for i in xrange(size):
+        for j in reversed(range(size)):
+            matrix_new[i][size-1-j] = matrix[j][i]
+    return matrix_new
