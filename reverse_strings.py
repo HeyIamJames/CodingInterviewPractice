d313d079f9548735e8cd5a91ba4f6aa92e357a6f
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-09-20 19:31:07 -0700

    Update reverse_strings.py

--- reverse_strings.py
+++ reverse_strings.py
@@ -3,3 +3,11 @@ make a function which:
 reverse a string such that "dog cat world"
 becomes "world cat dog"
 """
+
+"""
+psudo code
+store each word, break on white space
+return in reverse
+- possible to do in place by switching all word positions,
+then reverse recursively
+"""
