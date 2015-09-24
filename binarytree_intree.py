38c5bbd583046b02d09d94b30e3a5536c0658a2e
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-09-23 19:14:38 -0700

    Update binarytree_intree.py

--- binarytree_intree.py
+++ binarytree_intree.py
@@ -2,3 +2,26 @@
 find if a is a subset of b,
 where both a and b are binary trees
 """
+
+def tree_max(node):
+    if not node:
+        return float("-inf")
+    maxleft  = tree_max(node.left)
+    maxright = tree_max(node.right)
+    return max(node.key, maxleft, maxright)
+
+def tree_min(node):
+    if not node:
+        return float("inf")
+    minleft  = tree_min(node.left)
+    minright = tree_min(node.right)
+    return min(node.key, minleft, minright)
+
+def verify(node):
+    if not node:
+        return True
+    if (tree_max(node.left) <= node.key <= tree_min(node.right) and
+        verify(node.left) and verify(node.right)):
+        return True
+    else:
+        return False
