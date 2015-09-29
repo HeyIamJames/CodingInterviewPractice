c4ec70d6adf19877f3e9e3d75b48d70778c0be3e
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-09-29 13:58:10 -0700

    Update 3.2_stackmin.py

--- CrackingCodingInterview/3.2_stackmin.py
+++ CrackingCodingInterview/3.2_stackmin.py
@@ -2,3 +2,25 @@
 stack with pop, push, min
 opperate in O(1)
 """
+class StackMin:
+    def __init__(self):
+        self.stack = []
+        self.min = []
+        
+    def push(self, value):
+        self.stack.append(value)
+        if len(self.min) == 0 or value <= self.min[-1]:
+            self.min.append(value)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        data = self.stack.pop()
+        if data == self.min[-1]:
+            self.min.pop()
+        return data
+
+    def get_min(self):
+        if len(self.min)==0:
+            return None
+        return self.min[-1]
