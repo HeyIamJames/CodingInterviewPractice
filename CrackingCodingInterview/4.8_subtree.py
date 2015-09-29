be54a61932e9b021d6fb4dac942cfd0eb71a65f8
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-09-28 23:08:23 -0700

    Update 4.8_subtree.py

--- CrackingCodingInterview/4.8_subtree.py
+++ CrackingCodingInterview/4.8_subtree.py
@@ -3,24 +3,6 @@ is a a subtree of b
 both are bst
 """
 
-class Node:
-02
-    ...
-03
-    def compare_trees(self, node):
-04
-        """
-05
-        Compare 2 trees
-06
- 
-07
-        @param node tree's root node to compare to
-08
-        @returns True if the tree passed is identical to this tree
-09
-        """
-        
 def compare_trees(self, node):
     if node is None:
         return False
