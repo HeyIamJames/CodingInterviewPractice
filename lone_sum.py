4196c2cabe7a1e1aa83387209b7453e826bcf367
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-05 13:48:06 -0700

    Update lone_sum.py

--- lone_sum.py
+++ lone_sum.py
@@ -5,3 +5,15 @@ lone_sum(1, 2, 3) → 6
 lone_sum(3, 2, 3) → 2
 lone_sum(3, 3, 3) → 0
 """
+
+def lone_sum(a, b, c):
+  if a == b == c:
+    return 0
+  if b == c:
+    return a
+  if a == c:
+    return b
+  if a == b:
+    return c  
+  return a + b + c
+
