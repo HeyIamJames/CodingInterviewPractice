afeed417ad2d2603be4bd68d775a6dbe30cb0539
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-07 16:30:41 -0700

    Update monkey.py

--- monkey.py
+++ monkey.py
@@ -8,3 +8,12 @@ monkey_trouble(True, True) → True
 monkey_trouble(False, False) → True
 monkey_trouble(True, False) → False
 """
+
+def monkey_trouble(a_smile, b_smile):
+    if a_smile and b_smile:
+        return True
+    elif not a_smile and not b_smile:
+        return True
+    else:
+        return False
+  
