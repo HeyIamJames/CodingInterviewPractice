be040bb7db1b64c5e29c2a61ae59814203a3c832
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-14 16:43:31 -0700

    Update 5.3_nextsmallbig.py

--- CrackingCodingInterview/5.3_nextsmallbig.py
+++ CrackingCodingInterview/5.3_nextsmallbig.py
@@ -2,3 +2,5 @@
 given a positive integer, return the next smallest and largest
 number with the same number of 1s in the binary represenation
 """
+# next higher int =  switch the lowest '1' with the next higher '0'
+# next lowest int =  switch the highest '0' with the next higher '1'
