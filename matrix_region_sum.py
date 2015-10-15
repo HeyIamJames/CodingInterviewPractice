3a6570079be14b582a3e56b740880b6fe38f9560
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-14 17:26:03 -0700

    Update matrix_region_sum.py

--- matrix_region_sum.py
+++ matrix_region_sum.py
@@ -3,3 +3,12 @@ Given a matrix of integers and coordinates of a rectangular region within the ma
 find the sum of numbers falling inside the rectangle. 
 http://www.ardendertat.com/2011/09/20/programming-interview-questions-2-matrix-region-sum/
 """
+
+def matrixRegionSum1(matrix, A, D): 
+    if len(matrix)==0: 
+        return 
+    totalSum=0 
+    for i in range(A[0], D[0]+1):
+        for j in range(A[1], D[1]+1):
+            totalSum+=matrix[i][j] 
+    return totalSum
