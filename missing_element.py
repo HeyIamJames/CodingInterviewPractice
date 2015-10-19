cb29ce461eb143dc44b244576b153a0b7a3b1a7d
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-18 22:36:12 -0700

    Create missing_element.py

---
+++ missing_element.py
@@ -0,0 +1,6 @@
+"""
+There is an array of non-negative integers.
+A second array is formed by shuffling the elements of the first array and deleting a random element.
+Given these two arrays, find which element is missing in the second array.
+http://www.ardendertat.com/2012/01/09/programming-interview-questions/
+"""
