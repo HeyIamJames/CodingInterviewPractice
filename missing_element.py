82b826118fc2b6112e0d4b7dd9741bdae5eedeb3
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-19 21:35:18 -0700

    Update missing_element.py

--- missing_element.py
+++ missing_element.py
@@ -4,3 +4,9 @@ A second array is formed by shuffling the elements of the first array and deleti
 Given these two arrays, find which element is missing in the second array.
 http://www.ardendertat.com/2012/01/09/programming-interview-questions/
 """
+
+def findMissingNumber3(array1, array2):
+    result=0 
+    for num in array1+array2:
+        result^=num 
+    return result
