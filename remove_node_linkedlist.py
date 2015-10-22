2eabcf37ed550a384b160ef24b06061187876743
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-21 21:21:27 -0700

    Update remove_node_linkedlist.py

--- remove_node_linkedlist.py
+++ remove_node_linkedlist.py
@@ -2,3 +2,21 @@
 Given a linkedlist of integers and an integer value, 
 delete every node of the linkedlist containing that value. 
 """
+def delete_node(self,location):
+    if location == 0:
+        try:
+            self.cur_node = cur_node.next
+        except AttributeError:
+            self.cur_node = None
+        finally:
+            return 
+    node = self.cur_node        
+    try:
+        for _ in xrange(location):
+            node = node.next
+    except AttributeError:
+        raise ValueError("List does not have index {0}".format(location))
+    try:
+        node.next = node.next.next 
+    except AttributeError:
+        node.next = None
