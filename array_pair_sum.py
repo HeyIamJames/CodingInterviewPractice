8465cc11def20d94aef1297699869732520f6c6e
HeyIamJames <jameshemmaplardh@gmail.com>
2015-10-22 20:42:32 -0700

    linting and sample

--- array_pair_sum.py
+++ array_pair_sum.py
@@ -1,19 +1,25 @@
 """
-Given an integer array, output all pairs that sum up to a specific value k. 
+Given an integer array, output all pairs that sum
+up to a specific value k.
 http://www.ardendertat.com/2011/09/17/programming-interview-questions-1-array-pair-sum/
 """
 
-def pairSum(arr, k): 
-    if len(arr)<2: 
-        return 
-    arr.sort() 
-    left, right = (0, len(arr)-1) 
-    while left<right: 
-        currentSum=arr[left]+arr[right]
-        if currentSum==k:
-            print arr[left], arr[right] 
-            left+=1 
-        elif currentSum<k: 
-            left+=1 
-        else: 
-            right-=1
+def pairSum(arr, k):
+    if len(arr) < 2:
+        return
+    arr.sort()
+    left, right = (0, len(arr) - 1)
+    while left < right:
+        currentSum = arr[left] + arr[right]
+        if currentSum == k:
+            print arr[left], arr[right]
+            left += 1
+        elif currentSum < k:
+            left += 1
+        else:
+            right -= 1
+
+
+# example pass
+arr = [12, 134, 6, 9, 15, 10, 5, -10, 25, 300]
+k = 15
--- checkbst.py
+++ checkbst.py
@@ -1,9 +1,10 @@
 """
-This is a very common interview question. 
-Given a binary tree, check whether it’s a binary 
-search tree or not. Simple as that.. 
+This is a very common interview question.
+Given a binary tree, check whether it’s a binary
+search tree or not. Simple as that..
 http://www.ardendertat.com/2011/10/10/programming-interview-questions-7-binary-search-tree-check/
 """
+
 
 class Node:
     def __init__(self, val=None):
