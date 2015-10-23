b9e357e09f9aa706cbb078ac6b73f9142a6a0064
HeyIamJames <jameshemmaplardh@gmail.com>
2015-10-22 18:24:28 -0700

    fix array

--- largest_continuoussum_array.py
+++ largest_continuoussum_array.py
@@ -3,8 +3,12 @@ Given an array of integers (positive and negative) find the largest continuous s
 http://www.ardendertat.com/2011/09/24/programming-interview-questions-3-largest-continuous-sum
 """
 def largestContinuousSum(arr):
+    if len(arr) <= 0:
+        return ValueError
+    if len(arr) == 0:
+        return
     maxSum=currentSum=arr[0]
     for num in arr[1:]:
-        currentSum=max(currentSum+num, num) 
+        currentSum=max(currentSum+num, num)
         maxSum=max(currentSum, maxSum)
     return maxSum
