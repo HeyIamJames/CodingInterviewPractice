0f1ed7244d4f211d2e3359571fb3dcd746fb06c5
HeyIamJames <jameshemmaplardh@gmail.com>
2015-10-22 19:45:46 -0700

    start bst

---
+++ checkbst.py
@@ -0,0 +1,7 @@
+"""
+This is a very common interview question. 
+Given a binary tree, check whether it’s a binary 
+search tree or not. Simple as that.. 
+http://www.ardendertat.com/2011/10/10/programming-interview-questions-7-binary-search-tree-check/
+"""
+
