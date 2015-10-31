dbae53d23cd30ee2fbcfc04b0202f146dda7476e
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-10-30 18:14:12 -0700

    Update string_permutations.py

--- string_permutations.py
+++ string_permutations.py
@@ -8,15 +8,7 @@ http://www.ardendertat.com/2011/10/28/programming-interview-questions-11-all-per
 
 
 def permutate(string):
-    if len(string) == 1:
-        return string
-    perms = permutate(string[1:])
-    char = string[0]
-    result = []
-    for x in perms:
-        for i in range(len(x)+1):
-            result.append(x[:i] + char + x[i:])
-    return result
+
 
 #string = james, expected = ['james', 'ajmes', 'amjes', 'amejs', 'amesj']
 
