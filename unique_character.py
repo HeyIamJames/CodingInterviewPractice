8c9b494e3e838650355beb26540bd11fd5150a43
James Hemmaplardh <jameshemmaplardh@gmail.com>
2015-11-09 19:30:36 -0800

    Update unique_character.py

--- unique_character.py
+++ unique_character.py
@@ -2,5 +2,21 @@
 Find the first non-repeated (unique) character in a given string.
 """
 
-def unique(string):
+def counts(string):
+    count = [256]
+    for i in string:
+        count[ord(i)]+=1
+    return count
     
+def unique(string):
+    count = counts(string)
+    index = -1
+    k = 0
+ 
+    for i in string:
+        if count[ord(i)] == 1:
+            index = k
+            break
+        k += 1
+ 
+    return index
