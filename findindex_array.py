0d2050af66f1ea0a56f6f49010d8a1f369594db3
HeyIamJames <jameshemmaplardh@gmail.com>
2015-11-25 15:59:24 -0800

    start array

---
+++ findindex_array.py
@@ -0,0 +1,10 @@
+"""
+Given a sorted array of unknown length and a number to search for,
+return the index of the number in the array. Accessing an element out of bounds
+throws exception. If the number occurs multiple times, return the index of any
+occurrence. If it isn’t present, return -1.
+http://www.ardendertat.com/2011/11/21/programming-interview-questions-17-search-unknown-length-array/#sthash.SK6PWW8B.dp
+"""
+
+def getIndex():
+    
