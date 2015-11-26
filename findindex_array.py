127dca35fcfb7e4d8fa723af7243a95351555894
HeyIamJames <jameshemmaplardh@gmail.com>
2015-11-25 16:06:59 -0800

    simple solution

--- findindex_array.py
+++ findindex_array.py
@@ -6,5 +6,14 @@ occurrence. If it isn’t present, return -1.
 http://www.ardendertat.com/2011/11/21/programming-interview-questions-17-search-unknown-length-array/#sthash.SK6PWW8B.dp
 """
 
-def getIndex():
-    
+def getIndex(arr, val):
+    index = 0
+    if arr[index] == val:
+        return index
+    else:
+        index += 1
+
+"""
+arr = range(10, 50)
+val = 15
+"""
