d2983db8ffba772906ea25053a83f2f39399342a
HeyIamJames <jameshemmaplardh@gmail.com>
2017-05-12 21:39:07 -0700

    do not push

---
+++ 5problems.py
@@ -0,0 +1,11 @@
+"""
+Write three functions that compute the sum of the numbers in a given list using a for-loop, a while-loop, and recursion.
+"""
+
+def adding(li):
+    while True:
+        return sum(i for i in li)
+
+def adding2(li):
+    for i in len(li):
+        return sum(i for i in li)
